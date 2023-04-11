36fbf470702d9857b985d040ae44cc7231e1452d
unknown <eogbeide2@gmail.com>
2023-04-10 23:12:24 -0700

    added for loop to iterate and create cards with their respective header

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,13 +8,37 @@ var searchBtn = $('#btn-search');
 var historyLedger = [];
 
 // function to get the 5-day forecast
-var getFiveDay = function(lat, lon) {
+var getFiveDay = function(lat, lon, cityInput,) {
     var requestUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     $.ajax({
         url: requestUrl,
         method: `GET`
     }).then(function(response){
         console.log(response);
+        $('.card-header').empty();
+
+        var forecastHeader =$('<h3>').text('5-Day Forecast:');
+        var cardOutline =$('<div>').addClass('card-deck');
+        $('.card-title').empty();
+
+        for (var i = 0; i< response.list.length; i +=8){
+            var card = $('<div>').addClass('card mb-3');
+            var cardBody = $('<div>').addClass('card-body');
+            var cardTitle = $('<h4>').addClass('card-title').text(cityInput);
+            // var cardDate = $('<h5>').addClass('card-text text-center').text(JSON.parse(response.list[i].dt_txt).trim());
+            var temp = $('<p>').addClass('card-text').text(`Temp: ${response.list[i].main.temp} °F`);
+            var humidity = $('<p>').addClass('card-text').text(`Humidity: ${response.list[i].main.humidity}%`);
+
+            cardBody.append(cardTitle,temp,humidity);
+            card.append(cardBody);
+            cardOutline.append(card);
+            // cardBody.append(cardDate);
+
+
+        }
+
+        $('.card-body').append(forecastHeader);
+        $('.card-body').append(cardOutline);
     });
 };
 
@@ -41,8 +65,11 @@ var getCoord = function(city){
 
         localStorage.setItem(cityInput, JSON.stringify(data));
 
+        var history= $('<li>').addClass('list-group-item').text(cityInput);
+        historyList.append(history);
+
         // call the function to get the 5-day forecast
-        getFiveDay(lat, lon);
+        getFiveDay(lat, lon, cityInput);
     }).catch(function(error){
         console.log(error);
     });
